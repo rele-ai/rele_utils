eb43a457a9ecc7dd49c5d8605db5a3d618cda455
Saar Wintrov <saar@rele.ai>
2021-10-04 14:12:40 +0300

    This is change for the typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rele-utils",
+    name="releai-utils",
     version="0.0.1",
     author="Gal Shalom",
     author_email="gal@rele.ai",
