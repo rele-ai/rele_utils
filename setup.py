751c42feac4b3ef7cf58f21385dff64b50edf0fc
gal-sh <galshalom@Gals-MacBook-Pro.local>
2021-10-04 16:19:14 +0300

    change the package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="releai-utils",
+    name="UtilsTest",
     version="0.0.1",
     author="Gal Shalom",
     author_email="gal@rele.ai",
