2539e1bec07645c755f1bbf5c65f4fdfe4d80830
Saar Wintrov <saar@rele.ai>
2021-10-04 16:12:45 +0300

    This is change for req in package

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-import setuptools
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="releai-utils",
+setup(
+    name="releai",
     version="0.0.1",
     author="Gal Shalom",
     author_email="gal@rele.ai",
@@ -12,7 +12,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rele-ai/ops-utils",
-    packages=setuptools.find_packages(),
+    package_dir={ "utils": "utils" },
+    # packages=setuptools.find_packages(),
+    packages=["utils"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -23,5 +25,5 @@ setuptools.setup(
         "google-cloud==0.34.0",
         "google-cloud-container==2.7.1",
         "kubernetes==18.20.0",
-    ]
+    ],
 )
